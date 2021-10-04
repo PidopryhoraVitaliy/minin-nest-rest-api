78635a93b532adc628dbb0b2cd774320b990c26c
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2021-10-04 17:24:51 +0300

    38:00

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put, Redirect } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product-dto';
+import { UpdateProductDto } from './dto/update-product-dto';
 
 @Controller('products')
 export class ProductsController {
   @Get()
+  //@Redirect('https://google.com/', 301)
   getAll(): string {
     return 'getAll';
   }
+
   @Get(':id')
   getOne(@Param('id') id: string): string {
     return 'getOne ' + id;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
+  create(@Body() createProductDto: CreateProductDto): string {
+    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `Remove ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): string {
+    return `Update ${id} - Title: ${updateProductDto.title} Price: ${updateProductDto.price}`;
   }
 }
